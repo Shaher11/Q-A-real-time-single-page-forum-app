d87540f50ee948f32b8b5d687944ab9004c0b290
Ramadan <m7mdrama1245@gmail.com>
2019-07-29 01:05:55 +0200

    Create Migrations, Models, Controller and factories

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\like;
+use Faker\Generator as Faker;
+
+$factory->define(like::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# This is a real time single page forum app.
+
+## we are going to us Pusher, Laravel, Vuejs, Vuetify, JWT and Markdown.
